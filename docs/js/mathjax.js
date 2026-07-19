c1127607dc94b6e5b639b5399a00856f7c33ac90
Surya Sunkara <svns.surya@gmail.com>
2026-07-20 00:08:30 +0530

    Add MathJax support for rendering math equations in topic pages

---
+++ docs/js/mathjax.js
@@ -0,0 +1,24 @@
+/* Math equation rendering
+ * 
+ * Configures MathJax options globally before 
+ * the library loads
+ */ 
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
